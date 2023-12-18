deed867dd0aaa0044eb296d7b194d89ec9694565
myntp <119732035+MYNTCODE@users.noreply.github.com>
2023-12-19 00:24:37 +0700

    fix: responsive blog post

--- src/components/blog/BlogPost.jsx
+++ src/components/blog/BlogPost.jsx
@@ -7,15 +7,15 @@ function BlogPost() {
       <div className="Blog-Post">
         <div className="Blog-container space-y-10 mt-10 mb-20">
           {fashionBlog.map((blog, index) => (
-            <div className="blog" key={index}>
-              <div className=" md:flex justify-center md:px-[10%] text-center md:gap-10 ">
+            <div className="blog " key={index}>
+              <div className="h-[700px] md:h-full md:flex justify-center md:px-[10%] text-center md:gap-10 ">
                 <img
                   className=" md:w-[50%] md:h-[500px] object-cover"
                   src={blog.img}
                 />
                 <div className="BlogText mx-[10%] md:mx-0 h-[300px] mt-[10%] text-left">
                   <h1 className="text-[40px]"> {blog.title}</h1>
-                  <p>{blog.description}</p>
+                  <p className="">{blog.description}</p>
                 </div>
               </div>
             </div>
